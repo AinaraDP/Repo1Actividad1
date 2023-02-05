834a60719f217fcdfcc1d143e67cac800fc6e7e1
juancar.menendez <juancarmsanz@gmail.com>
2023-02-05 16:53:08 +0100

    solventar error

--- Repo1Actividad1/src/Eric/Eric.java
+++ Repo1Actividad1/src/Eric/Eric.java
@@ -10,7 +10,6 @@ public class Eric {
 	  public void imprimirNombre() {
 	    System.out.println("Hola, soy " + this.nombre);
 	    System.out.println("Mis hobbies: leer comics, hacer ejercicio y ver peliculas");
-	    System.out.println("Mis comidas favoritas son: Brocoli, calabacín y lechuga");
 	  }
 
 	  
